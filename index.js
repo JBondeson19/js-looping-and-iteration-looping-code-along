fbb54c2ad4df513d3821b04e71687299d1726647
JBondeson19 <jpbond58@gmail.com>
2020-11-17 00:00:04 -0500

    "for/while"

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+
+function writeCards(nameArray, event) {
+    let thankYouCards = []
+for (let i = 0; i < nameArray.length; i++) {
+    thankYouCards.push(`Thank you, ${nameArray[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYouCards
+}
+
+
+function countDown(num) {
+  while (num > 0 ) {
+      console.log(num);
+      num -=1;
+  }
+  console.log(num);
+}
